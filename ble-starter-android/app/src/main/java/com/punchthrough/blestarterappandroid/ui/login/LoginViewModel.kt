44d8aa93c71759357fe3afed3c3218a54763a3c3
George Holly-Greco <ghollygr@uwaterloo.ca>
2024-07-21 12:49:43 -0400

    Combine changes

---
+++ ble-starter-android/app/src/main/java/com/punchthrough/blestarterappandroid/ui/login/LoginViewModel.kt
@@ -0,0 +1,55 @@
+package com.punchthrough.blestarterappandroid.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import com.punchthrough.blestarterappandroid.data.LoginRepository
+import com.punchthrough.blestarterappandroid.data.Result
+
+import com.punchthrough.blestarterappandroid.R
+
+class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+    private val _loginForm = MutableLiveData<LoginFormState>()
+    val loginFormState: LiveData<LoginFormState> = _loginForm
+
+    private val _loginResult = MutableLiveData<LoginResult>()
+    val loginResult: LiveData<LoginResult> = _loginResult
+
+    fun login(username: String, password: String) {
+        val result = loginRepository.login(username, password)
+        if (result is Result.Success) {
+            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+        } else {
+            _loginResult.value = LoginResult(error = R.string.login_failed)
+        }
+    }
+
+    fun loginDataChanged(username: String, password: String) {
+        // Commenting out validation
+        /*
+        if (!isUserNameValid(username)) {
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+        } else if (!isPasswordValid(password)) {
+            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
+        } else {
+            _loginForm.value = LoginFormState(isDataValid = true)
+        }
+        */
+        // Always set data valid to true for testing purposes
+        _loginForm.value = LoginFormState(isDataValid = true)
+    }
+
+    private fun isUserNameValid(username: String): Boolean {
+        return if (username.contains('@')) {
+            Patterns.EMAIL_ADDRESS.matcher(username).matches()
+        } else {
+            username.isNotBlank()
+        }
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 5
+    }
+}
